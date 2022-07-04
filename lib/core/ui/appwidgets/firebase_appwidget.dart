34851b479234466c11e9257353faf557170fd066
Daniel Souza <sp.daniell@live.com>
2022-07-04 12:17:12 -0300

    Added bloc state management

--- lib/core/ui/appwidgets/firebase_appwidget.dart
+++ lib/core/ui/appwidgets/firebase_appwidget.dart
@@ -1,33 +1,44 @@
+import 'package:fadu/core/data/repositories/firebase_repository.dart';
 import 'package:fadu/core/data/sources/firebase/firebase_options.dart';
+import 'package:fadu/core/ui/bloc/app_state.dart';
 import 'package:fadu/core/ui/shared/app_info_data.dart';
+import 'package:fadu/domain/repositories/messages_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import '../shared/app_info_data.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FirebaseAppWidget extends StatelessWidget {
   const FirebaseAppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    MessagesRepository repository = FirebaseRepository();
     return FutureBuilder(
-      future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform),
-      builder: ((context, initializeSnapshot) {
-        if (initializeSnapshot.connectionState == ConnectionState.waiting) {
-          return const SplashPage();
-        }
-        if (initializeSnapshot.connectionState == ConnectionState.done) {
-          return StreamBuilder(builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              //OBS: HAVE TO SEND SNAPSHOT TO THE BLOC PROVIDER
-            }
-            return const HomePage();
-          });
-        }
-        return Container(
-          color: Colors.black,
-        );
-      }),
-    );
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, initSnapshot) {
+          if (initSnapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              color: Colors.amber,
+            );
+          }
+          if (initSnapshot.connectionState == ConnectionState.done) {
+            return StreamBuilder(
+                stream: repository.fetch(userName: 'user1ID'),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const HomePage();
+                  }
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    return BlocProvider(
+                        create: (_) => AppState(snapshot),
+                        child: const LoginPage());
+                  }
+                  return const SizedBox();
+                });
+          }
+
+          return const SizedBox();
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:fadu/core/ui/appwidgets/firebase_appwidget.dart';
 import 'package:fadu/core/ui/shared/app_info_data.dart';
 import 'package:flutter/material.dart';
 
-import 'core/ui/appwidgets/firebase_appwidget.dart';
-
 AppInfoData appInfo = AppInfoData();
-const appWidget = FirebaseAppWidget();
-void main() => runApp(
-    MaterialApp(title: appInfo.title, routes: appInfo.routes, home: appWidget));
+
+void main() => runApp(MaterialApp(
+    title: appInfo.title,
+    routes: appInfo.routes,
+    home: const FirebaseAppWidget()));
