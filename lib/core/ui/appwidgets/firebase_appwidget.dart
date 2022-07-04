b5afab34afdbaa24711328ed2d6be565d894960c
Daniel Souza <sp.daniell@live.com>
2022-07-03 23:13:47 -0300

    firebase_appwidget

--- lib/core/ui/appwidgets/firebase_appwidget.dart
+++ lib/core/ui/appwidgets/firebase_appwidget.dart
@@ -1,3 +1,6 @@
+import 'package:fadu/core/data/sources/firebase/firebase_options.dart';
+import 'package:fadu/core/ui/shared/app_info_data.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseAppWidget extends StatelessWidget {
@@ -6,9 +9,31 @@ class FirebaseAppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform),
       builder: ((context, snapshot) {
-        return Scaffold(
-          appBar: AppBar(),
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.black,
+            child: const SizedBox(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.black,
+                color: Colors.red,
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const SplashPage();
+        }
+        return Container(
+          color: Colors.black,
+          child: const SizedBox(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.black,
+              color: Colors.red,
+            ),
+          ),
         );
       }),
     );
--- lib/core/ui/shared/app_info_data.dart
+++ lib/core/ui/shared/app_info_data.dart
@@ -17,7 +17,12 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('');
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+      ),
+    );
   }
 }
 
